8bc25056a5db81c0f1a116ea6abe5adf8cd51f5b
Holison <kobbyholison2002@gmail.com>
2023-05-29 14:28:36 +0000

    If else conditions

---
+++ Conditional Statement/if_else.py
@@ -0,0 +1,34 @@
+# Python if else statement is used for making decision.
+# If a certain condition then perform a certain task else perform another task
+
+# for example
+
+number = int(input("Enter a number: "))
+
+# an if statement
+if number > 0:
+    print("The number is positive")
+
+
+# but in some cases we want to evaluate or perform another task if something goes wrong and hence
+# we use the if else
+
+number = int(input("Enter a number: "))
+
+# if else block
+if number > 0:
+    print("The number is positive")
+else:
+    print("The number is negative")
+
+
+# we can also have other conditions and hence we use the if elif and else statement
+number = int(input("Enter a number: "))
+
+# if,elif and else block
+if number > 0:
+    print("The number is positive")
+elif number < 0:
+    print("The number is negative")
+else:
+    print("Other")
