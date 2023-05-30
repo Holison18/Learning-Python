55936519212ea7b337e3d9cdad5c65776954cd3e
Holison <kobbyholison2002@gmail.com>
2023-05-30 13:06:49 +0000

    while loops

---
+++ Conditional Statement/while_loops.py
@@ -0,0 +1,13 @@
+# Python while loops are use to perform a task over and over again until a condition is false
+'''
+    syntax:
+    while condition:
+        statement(s)
+'''
+
+# basic example
+n = 1
+
+while n <= 5:
+    print('Hello World!')
+    n+=1
