33f36e8f33d3bc78a8555f22d63dc7647baddd05
Holison <kobbyholison2002@gmail.com>
2023-05-30 13:38:42 +0000

    break and continue statement

---
+++ Conditional Statement/break_and_continue.py
@@ -0,0 +1,23 @@
+# The break statement is use to terminate a loop when a certain condition is met or encountered
+# break statements can be used in both for and while loops
+
+# for loop with break
+print('Using break with a for loop')
+n = 10
+for i in range(n):
+    if i==4:
+        break
+    print(i)
+
+# while loop with break
+print('Using a while loop with a break')
+
+while n >= 0:
+    if n==4:
+        break
+    print(n)
+    n -= 1
+
+
+# The continue statement is used to skip the current iteration.
+# The flow of the program goes to the next iteration
