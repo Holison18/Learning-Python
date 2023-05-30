f082469d36bcc50a2891be5b493a288e34ae456a
Holison <kobbyholison2002@gmail.com>
2023-05-30 13:19:06 +0000

    While loop

--- Conditional Statement/while_loops.py
+++ Conditional Statement/while_loops.py
@@ -11,3 +11,19 @@ n = 1
 while n <= 5:
     print('Hello World!')
     n+=1
+
+
+# a simple while loop to add numbers whiles the user provides input
+isRun = True
+Total = 0
+while isRun:
+    num1 = int(input('Enter a number: '))
+    Total += num1
+    ask = input("Do you want to add another number? y/n: ")
+    if ask == 'y':
+        continue
+    else:
+        isRun == False
+        break
+print(f"Total: {Total}")
+
