35f8a6431097fbe45ff60532b5d1304a53c2be81
Holison <kobbyholison2002@gmail.com>
2023-06-20 10:10:06 +0000

    pass statement

---
+++ Conditional Statement/pass_statement.py
@@ -0,0 +1,14 @@
+# assuming we implement a function or a conditional statement and we do not want to do anything, we can use pass statement to avoid getting an error.
+# pass statement for conditional statement
+
+number = int(input("Enter a number: "))
+if number > 10:
+    pass
+else:
+    pass
+
+
+#pass statement for functions
+
+def my_funct(num):
+    pass
